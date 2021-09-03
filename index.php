adb9606228e396fdd37bf083fd1357084c226bdf
MOHD TAYYAB <fictitioushunt@gmail.com>
2021-09-03 14:40:43 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,83 @@
+
+<?php  include 'header.php';?>
+
+
+
+</div>
+
+
+<div class="container">
+  <div class="jumbotron">
+         
+
+
+
+
+<h2>Login</h2>
+
+
+
+  <form class="form-horizontal" role="form" method="post" action="p_index.php">
+
+    <table>
+
+<tr>
+  <div class="form-group">
+  <label for="name" class="col-sm-2 control-label">User name</label> 
+   
+  <div class="col-sm-5">
+  <input type="text" class="form-control" id="name" name="name" placeholder="amazon user name"></td>
+  </div>
+</tr>
+
+
+  <tr>
+
+
+    <div class="form-group">
+    <label for="name" class="col-sm-2 control-label">Password</label>
+   
+    <div class="col-sm-5">
+    <input type="password" class="form-control" id="name" name="pass" placeholder="password">
+    </div>
+  </tr>
+
+
+
+<br><br><br>
+   <div class="form-group">
+    <div class="col-sm-11 col-sm-offset-2">
+    <input id="submit" name="sub1" type="submit" value="Login" class="btn btn-primary">
+     </div>
+
+
+   
+   
+   
+   
+   
+   
+</form>
+
+
+
+</div>
+
+<div class="alert alert-danger">
+  <strong>Note!</strong> 
+</div>
+
+
+
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
